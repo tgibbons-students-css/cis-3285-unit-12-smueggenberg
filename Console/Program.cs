12db1bb7f996d1bb8fbc2f464ffb91f1dc7aa8b8
smueggenberg <smueggenberg@css.edu>
2016-11-22 11:05:08 -0600

    Change program to create a user repository instead of a entity repository.

--- Console/Program.cs
+++ Console/Program.cs
@@ -12,14 +12,26 @@ namespace ConsoleSample
     {
         static void Main(string[] args)
         {
-
-            EntityRepository entityRepository = new EntityRepository();
+            /*
+            IEntityRepository<Entity> entityRepository = new EntityRepository();
             Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
 
             myEntity.Name = "Tom";
             Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
             Console.WriteLine("New entity Name: " + myEntity.Name);
+            Console.ReadKey();
+            */
 
+            //UserRepository entityRepository = new UserRepository();
+            IEntityRepository<User> userRepository = new UserRepository();
+            User myUser = userRepository.GetByID(Guid.NewGuid());
+
+            myUser.Name = "Steve";
+            myUser.EmailAddress = "smueggenberg@css.edu";
+            Console.WriteLine("New user ID: " + myUser.ID.ToString());
+            Console.WriteLine("New user Name: " + myUser.Name);
+            Console.WriteLine("New user Email: " + myUser.EmailAddress);
+            Console.ReadKey();
         }
     }
 }
