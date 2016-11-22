5742f2f9d1f691231fe8293b12ff2bc78cc6c59f
smueggenberg <smueggenberg@css.edu>
2016-11-22 10:48:40 -0600

    Implement IEntityRepository

---
+++ SubtypeCovariance/IEntityRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SubtypeCovariance
+{
+    public interface IEntityRepository<TEntity>
+        where TEntity : Entity
+    {
+        TEntity GetByID(Guid id);
+    }
+}
